7035d7f3132225ec8eb7c9db02d2d3b46d3962b0
Eamonn-O-Flaherty <90116324+Eamonn-O-Flaherty@users.noreply.github.com>
2021-10-02 19:00:31 +1000

    Add files via upload

---
+++ Demos files/scripts/testing-area.js
@@ -0,0 +1,12 @@
+/* TESTING AREA */
+
+<script src="scripts/testing-area.js" defer></script>
+
+
+
+// You could optionally even do a little bit of string templating
+/*div.innerHTML = div.innerHTML
+    .replace(/{VENDOR}/g, 'ACME Inc.')
+    .replace(/{PRODUCT}/g, 'Best TNT')
+    .replace(/{PRICE}/g, '$1.49');*/
+
